b8f1797e5da77e6c3518cec3417c50260241ae44
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-11-07 19:56:47 +0100

    Add files via upload

---
+++ js/discord.js
@@ -0,0 +1,1 @@
+function myFunction(){var e=document.getElementById("myInput");e.select(),e.setSelectionRange(0,99999),navigator.clipboard.writeText(e.value),swal({title:"Sukces!",text:"Skopiowano mojego discorda: gufonnn",icon:"success",button:"Gotowe",dangerMode:!1})}
